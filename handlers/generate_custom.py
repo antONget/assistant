8767439522d2712323b9fa72b5603091d0482922
antONget <a.l.ponomare@mail.ru>
2025-03-07 18:06:09 +0300

    version 2.0

--- handlers/generate_custom.py
+++ handlers/generate_custom.py
@@ -1,4 +1,4 @@
-from aiogram import Router, F
+from aiogram import Router, F, Bot
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
@@ -6,6 +6,7 @@ from aiogram.filters import CommandStart, StateFilter, or_f
 from config_data.config import Config, load_config
 import qrcode
 from PIL import Image
+import os
 import logging
 
 router = Router()
@@ -13,6 +14,28 @@ config: Config = load_config()
 
 class LinkQr(StatesGroup):
     link_qr = State()
+
+
+DOWNLOADS_FOLDER = 'QR'
+
+async def download_photo(message: Message, bot: Bot):
+    # Проверяем, есть ли файл в сообщении
+    if message.photo:
+        # Получаем информацию о фотографии
+        photo_info = message.photo[-1]
+        # Получаем объект File для скачивания
+        photo_file = await bot.get_file(photo_info.file_id)
+        # Ссылка для скачивания файла
+        photo_url = f'https://api.telegram.org/file/bot{config.tg_bot.token}/{photo_file.file_path}'
+        # Создаём папку для сохранения файлов, если её нет
+        if not os.path.exists(DOWNLOADS_FOLDER):
+            os.makedirs(DOWNLOADS_FOLDER)
+        # Загружаем фотографию в локальную папку
+        photo_path = os.path.join(DOWNLOADS_FOLDER, f'{message.from_user.id}.jpg')
+        await photo_file.download(photo_path)
+        # Далее можно провести необходимую обработку или отправить ответ пользователю
+        await message.reply(f'Фотография сохранена: {photo_path}')
+
 
 
 @router.message(F.text == '/QR')
@@ -23,7 +46,7 @@ async def all_message(message: Message, state: FSMContext) -> None:
 
 
 @router.message(F.photo, StateFilter(LinkQr.link_qr))
-async def all_message(message: Message, state: FSMContext) -> None:
+async def all_message(message: Message, state: FSMContext, bot: Bot) -> None:
     # 🔹 Данные для QR-кода (ссылка или текст)
     data = message.text
 
@@ -41,7 +64,8 @@ async def all_message(message: Message, state: FSMContext) -> None:
     qr_img = qr.make_image(fill="black", back_color="white").convert("RGB")
 
     # 🔹 Добавление логотипа (если есть)
-    logo_path = "logo.png"  # Укажите свой логотип
+    await download_photo(message=message, bot=bot)
+    logo_path = f"{message.from_user.id}.png"  # Укажите свой логотип
     try:
         logo = Image.open(logo_path)
 
